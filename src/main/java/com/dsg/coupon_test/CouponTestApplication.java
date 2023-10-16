8f7b7cf05cb5ad4bf6df5a047e5e4d8c4a229267
seonggon do <mooh2jj@naver.com>
2023-10-16 09:18:53 +0900

    update: scheduler 추가

--- src/main/java/com/dsg/coupon_test/CouponTestApplication.java
+++ src/main/java/com/dsg/coupon_test/CouponTestApplication.java
@@ -2,7 +2,9 @@ package com.dsg.coupon_test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class CouponTestApplication {
 
---
+++ src/main/java/com/dsg/coupon_test/schedule/ScheduleTask.java
@@ -0,0 +1,21 @@
+package com.dsg.coupon_test.schedule;
+
+import com.dsg.coupon_test.service.AdminCouponService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ScheduleTask {
+
+    private final AdminCouponService adminCouponService;
+    @Scheduled(cron = "0 0 0 * * *")       // 매일 자정 12시에 check
+    public void couponExpireCheck() {
+        log.info("coupon expire check run...");
+//        adminCouponService.expireCouponRecord();
+    }
+
+}
